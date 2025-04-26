184d410605b090ce1f52958852b224d6d1410b23
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2025-04-26 18:21:25 +1000

    Sidebar - Fixed content not changing size dynamically with the window screen.

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,21 +1,19 @@
 import Navbar from "@/components/Navbar";
 import AppSidebar from "@/components/AppSidebar";
 import React from "react";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <SidebarProvider>
+      <SidebarProvider className="flex flex-col">
+        <Navbar />
         <div className="flex flex-1">
-          <div className="w-1/6 hidden md:block">
-            <AppSidebar />
-          </div>
-          <div className="grow p-5 md:w-[1140px] pt-24">{children}</div>
+          <AppSidebar />
+          <SidebarInset>
+            <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+          </SidebarInset>
         </div>
       </SidebarProvider>
-    </div>
   );
 };
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ import ThemeToggler from "@/components/ThemeToggler";
 
 const Navbar = () => {
   return (
-    <div className="fixed z-10 w-full bg-primary dark:bg-slate-700 py-4 px-5 flex justify-between text-white">
+    <div className="flex sticky top-0 z-10 w-full h-[--header-height] bg-primary dark:bg-slate-700 py-4 px-5 justify-between text-white">
       <Link href="/">
         <Image src={logo} alt="Sentigraph" width={40} className="rounded-xl" />
       </Link>
